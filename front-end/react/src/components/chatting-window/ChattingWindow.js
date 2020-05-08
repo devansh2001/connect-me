a19505a1453db432e0fe7c2dd2f399ef7024a54a
devansh2001 <dponda3@gatech.edu>
2020-05-07 23:35:29 -0400

    Basic structure for left pane setup

--- front-end/react/src/components/chatting-window/ChattingWindow.js
+++ front-end/react/src/components/chatting-window/ChattingWindow.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import TypeMessageBox from "./right-pane/TypeMessageBox";
+import Contact from "./left-pane/Contact";
+import {Row, Col, Container} from 'react-bootstrap';
 
 class ChattingWindow extends Component {
     render() {
         return (
             <div className={'chatting-window'}>
-                {/* insert the current user info component from issue 21 here*/}
-                {/* insert the recent chats component from issue 22 here*/}
-                <TypeMessageBox/>
+                <Container>
+                    <Row>
+                        <Col xs={3}>
+                            <Contact/>
+                        </Col>
+                        <Col xs={9}>
+                            {/* insert the current user info component from issue 21 here*/}
+                            {/* insert the recent chats component from issue 22 here*/}
+                            <TypeMessageBox/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
---
+++ front-end/react/src/components/chatting-window/left-pane/Contact.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Hello, World!
+            </div>
+        );
+    }
+
+}
+
+export default Contact;
