e5c39505382cbd209962fbffbe011a6725883c6c
devansh2001 <dponda3@gatech.edu>
2020-05-07 20:31:26 -0400

    Added message logging on clicking send

--- front-end/react/src/components/chatting-window/ChattingWindow.js
+++ front-end/react/src/components/chatting-window/ChattingWindow.js
@@ -4,8 +4,9 @@ import TypeMessageBox from "./right-pane/TypeMessageBox";
 class ChattingWindow extends Component {
     render() {
         return (
-            <div>
-                Hello, World!
+            <div className={'chatting-window'}>
+                {/* insert the current user info component from issue 21 here*/}
+                {/* insert the recent chats component from issue 22 here*/}
                 <TypeMessageBox/>
             </div>
         )
--- front-end/react/src/components/chatting-window/right-pane/TypeMessageBox.js
+++ front-end/react/src/components/chatting-window/right-pane/TypeMessageBox.js
@@ -1,15 +1,44 @@
 import React, {Component} from 'react';
+import {Form, Col, Button} from 'react-bootstrap';
 
 
 class TypeMessageBox extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            message: ''
+        }
     }
+
+    handleSubmit = async (e) => {
+        console.log(this.state.message);
+    };
+
+    handleChange = (e) => {
+        let currMessage = e.target.value;
+        this.state = {
+            message: currMessage
+        }
+    };
 
     render() {
         return (
-            <div>
-                Hello, World!
+            <div className={'type-message-box'}>
+                <Form >
+                    <Form.Row>
+                        <Col>
+                            <Form.Control
+                                onChange={this.handleChange}
+                                as={"textarea"}
+                                placeholder={"Type your message..."}
+                                rows={3}
+                            />
+                        </Col>
+                        <Col lg={1}>
+                            <Button onClick={this.handleSubmit} variant={'primary'}>Send</Button>
+                        </Col>
+                    </Form.Row>
+                </Form>
             </div>
         )
     }
