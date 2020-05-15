f9b9d141f7f1f6abd0d4b300fbae8bd27b7f7e7f
devansh2001 <dponda3@gatech.edu>
2020-05-15 18:57:40 -0400

    Parsed data from body params, added query validation

--- back-end/routes/SendMessage.js
+++ back-end/routes/SendMessage.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
 
+isEmpty = (data) => {
+    return data == null || data.length === 0;
+};
+
 router.route('/').post((request, response) => {
-    response.json({'response' : 'Send Message Endpoint'});
+    console.log('Received Data:');
+    console.log(request.body);
+
+    var message = request.body['message'];
+    var fromUser = request.body['sender'];
+    var toUser = request.body['receiver'];
+
+    // Check if input received is valid
+    if (isEmpty(message) || isEmpty(fromUser) || isEmpty(toUser)) {
+        let errNum = 400;
+        var json = {
+            'error' : errNum,
+            'message' : 'Please check your API request!'
+        };
+        response.status(errNum).send(json);
+        return;
+    }
+
+    // Code to check database if both sender and receiver exist
+
+
+
+
+    // Code to POST this message to the database
+
+
+
+    // ISSUE #11
+    // Code to use web sockets to send this message to the receiver
+
+
+    // Success message
+    response.status(201).send();
 });
 
 module.exports = router;
--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+var bodyParser = require('body-parser');
 // to use environment variables; we may not need it but just in case
 require('dotenv').config();
 
@@ -13,6 +13,10 @@ app.use(cors());
 // lets app use JSON to communicate with client(s)
 app.use(express.json());
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+   extended: true
+}));
 const testRouter = require('./routes/test');
 const sendMessageRouter = require('./routes/SendMessage');
 
