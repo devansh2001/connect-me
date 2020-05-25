49e998ddac450029c5f3d042828626c1c464c832
devansh2001 <dponda3@gatech.edu>
2020-05-24 23:32:43 -0400

    Added socket.io

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 var bodyParser = require('body-parser');
+const http = require('http');
+const socketIO = require('socket.io');
 // to use environment variables; we may not need it but just in case
 require('dotenv').config();
 
@@ -9,6 +11,9 @@ const port = 8080;
 
 // does something with security
 app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIO(server);
 
 // lets app use JSON to communicate with client(s)
 app.use(express.json());
@@ -23,7 +28,16 @@ const sendMessageRouter = require('./routes/SendMessage');
 app.use('/test', testRouter);
 app.use('/send-message', sendMessageRouter);
 
+io.on('connection', socket => {
+   console.log('Client connected');
+
+   socket.on('disconnect', () => {
+      console.log('Client disconnected');
+   })
+});
+
+
 // starts listening
-app.listen(port, () => {
+server.listen(port, () => {
    console.log('ConnectMe Server is running on port: ' + port);
 });
