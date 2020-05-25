30f696b214e5933196a804bc2c012dd133590447
Varun Kulkarni <kulkarni.varun22@gmail.com>
2020-05-25 16:58:56 -0400

    adding modified server.js as a new commit in case things need to backtrack on merge

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // to use environment variables; we may not need it but just in case
 require('dotenv').config();
@@ -7,14 +8,23 @@ require('dotenv').config();
 const app = express();
 const port = 8080;
 
-// does something with security
+//mongodb atlas configuration
+const connectionString = "mongodb+srv://connect123:connect123@connect-me-xh3xg.mongodb.net/test?retryWrites=true&w=majority";
+const connector = mongoose
+   .connect(connectionString, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true})
+   .then(() => console.log("Connected to MongoDB Atlas"))
+   .catch((err) => {console.log("Couldn't connect to MongoDB Atlas: ", err)});
+
+// enable cross origin resource sharing so that server can be acessed from anywhere
 app.use(cors());
 
 // lets app use JSON to communicate with client(s)
 app.use(express.json());
 
 const testRouter = require('./routes/test');
+const dbTestRouter = require('./routes/test-db');
 app.use('/test', testRouter);
+app.use('/test-db', dbTestRouter);
 
 // starts listening
 app.listen(port, () => {
