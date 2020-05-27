693f1201b72446a7efcb19ff7f558e3ff94f0946
vtrivedi7 <45473757+vtrivedi7@users.noreply.github.com>
2020-05-27 18:40:24 -0400

    Overlapping Sructure to deploy
    DIrectories Cliet and Server contain skeletop express and react apps. All other files need to be appropriately allocated within these folders AFTER the app is successfully deployed. Do not merge with master yet

---
+++ Server/build/precache-manifest.89652a3f70117979812baf0bd79c1a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3ed9d8ddcaa38a154811851afefab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6412f7ef97f72cc93ecc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b50ec7e4be1f7f600f05",
+    "url": "/static/js/2.22df4c65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22df4c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6412f7ef97f72cc93ecc",
+    "url": "/static/js/main.4fe4b849.chunk.js"
+  },
+  {
+    "revision": "ec7c7f6f6d01894df1c8",
+    "url": "/static/js/runtime-main.23b17bca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
