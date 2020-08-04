c0b48093eeddbbb57bf1ccbb745786fba08ba95d
devansh2001 <dponda3@gatech.edu>
2020-08-03 21:09:52 -0400

    connected react-chat-ui to messages sent/received

--- front-end/react/src/components/chatting-window/ChattingWindow.js
+++ front-end/react/src/components/chatting-window/ChattingWindow.js
@@ -7,7 +7,7 @@ import {Row, Col, Container, Button} from 'react-bootstrap';
 import socketIOClient from "socket.io-client";
 import SearchBar from "./left-pane/SearchBar";
 import ConnectMe from "./left-pane/ConnectMe"
-
+import { ChatFeed, Message, ChatBubbleProps } from 'react-chat-ui'
 
 class ChattingWindow extends Component {
 
@@ -22,7 +22,9 @@ class ChattingWindow extends Component {
                 'to': '',
                 'message': ''
             },
-            currentChatUsername: name
+            currentChatUsername: name,
+            allMessages: [],
+            forceReload: false
             // socket = socketIOClient(this.state.endpoint, {
             //     'query': this.state.username
             // })
@@ -47,6 +49,11 @@ class ChattingWindow extends Component {
             }
         });
         await this.eventSender();
+        await this.updateAllMessages({
+            'message': this.state.messageInfo.message,
+            'from': this.state.username.name,
+            'to': this.state.currentChatUsername
+        });
     };
 
     eventSender = () => {
@@ -59,21 +66,54 @@ class ChattingWindow extends Component {
         this.socket.emit('message_to_server', info);
     };
 
+    updateAllMessages = async (message) => {
+        let curr = this.state.allMessages;
+
+        let id = 1;
+        console.log(message['from']);
+        console.log(this.state.username.name);
+        if (message['from'] === this.state.username.name) {
+            console.log("ACCHA");
+            id = 0;
+        }
+
+        let formatted = new Message({
+            id: id,
+            message: message.message
+        });
+
+        if (curr[this.state.currentChatUsername]) {
+            curr[this.state.currentChatUsername].push(formatted);
+        } else {
+            curr[this.state.currentChatUsername] = [];
+            curr[this.state.currentChatUsername].push(formatted);
+        }
+        // await curr[this.state.currentChatUsername].push(message);
+        let initialForce = this.state.force;
+        await this.setState({
+            allMessages: curr,
+            forceReload: !initialForce
+        });
+        // await this.formatMessages();
+        await console.log(this.state.allMessages);
+    };
 
     render() {
 
         // this.socket.on('my_event', () => {
         //     console.log('Event Registered by client');
         // });
-        this.socket.on('message_to_client', (info) => {
+        this.socket.on('message_to_client', async (info) => {
             console.log(info);
             console.log('message');
+            await this.updateAllMessages(info);
         });
         this.socket.on('magic_event', (idx) => {
             console.log("Known: " + this.socket.id);
             console.log("New: " + idx);
         });
 
+        console.log(this.state.allMessages[this.state.currentChatUsername]);
         return (
             <div className={'chatting-window'}>
                 {/*<Container>*/}
@@ -87,8 +127,14 @@ class ChattingWindow extends Component {
                             </Row>  
                         </Col>
                         <Col xs={9}>
+                            {console.log('GOING IN THERE')}
                             {/* insert the current user info component from issue 21 here*/}
-                            <CurrentChat />
+                            <CurrentChat messages={this.state.allMessages[this.state.currentChatUsername] ?
+                                this.state.allMessages[this.state.currentChatUsername] : []}
+                                         username={this.state.username}
+                                         force={this.state.forceReload}
+                            />
+                            <hr/>
                             <TypeMessageBox sendMessageHandler={this.updateMessageInfo}/>
                         </Col>
                     </Row>
