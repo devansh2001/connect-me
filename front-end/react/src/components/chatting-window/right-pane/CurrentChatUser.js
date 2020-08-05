0e17c49aaacafcc3694a2f7d341b133e498b7353
pravi34 <prernaravi@gatech.edu>
2020-08-04 20:21:49 -0700

    Added profile pic to current chat

--- front-end/react/src/components/chatting-window/right-pane/CurrentChatUser.js
+++ front-end/react/src/components/chatting-window/right-pane/CurrentChatUser.js
@@ -1,34 +1,46 @@
 import React, {Component} from 'react';
 import {Container, Col, Image, Row} from 'react-bootstrap';
+import devanshPhoto from "./../ProfilePhotos/devanshPhoto.png";
+import prernaPhoto from "./../ProfilePhotos/prernaPhoto.png";
+import vatsalPhoto from "./../ProfilePhotos/vatsalPhoto.png";
+import varunPhoto from "./../ProfilePhotos/varunPhoto.png";
+import burdellPhoto from "./../ProfilePhotos/burdellPhoto.png";
 
 class CurrentChatuser extends Component {
     render() {
         const userName= this.props.currentChatUsername;
-        let fullName= "";
+        let fullName = "";
+        let photo = {devanshPhoto}
         switch(userName) {
             case "dponda":
                 fullName = "Devansh Ponda"
+                photo = devanshPhoto
                 break;
             case "vtrivedi":
                 fullName = "Vatsal Trivedi"
+                photo = vatsalPhoto
                 break;
             case "vkulkarni":
                 fullName = "Varun Kulkarni"
+                photo = varunPhoto
                 break;
             case "pravi":
                 fullName = "Prerna Ravi"
+                photo = prernaPhoto
                 break;
             case "THWG":
                 fullName = "George P Burdell"
+                photo = burdellPhoto
                 break;
             default: 
                 fullName = "User's Full Name Here"
+                photo = devanshPhoto
         }
         return (
             <Container className={'current-chat-user'} >
                 <Row>
                 <Col xs={1} >
-                            <Image className={'contact-image-current'} roundedCircle src="https://president.gatech.edu/sites/default/files/images/cabrera-headshot.jpg" fluid />
+                            <Image className={'contact-image-current'} roundedCircle src={photo} fluid />
                 </Col>
                 <Col >
                     <h4 className={'current-user-header'}>{fullName} ({userName})</h4>
